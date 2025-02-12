93a427e7a813c976747f97a7629cd3e07d2baa21
Mihail Alinskij <alinskij@proton.me>
2025-02-12 15:27:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Installing dependencies
 
 cd; echo -e "\e[1;32mInstalling dependencies...\e[m"
-sudo pacman -S wget nerd-fonts qt5-wayland qt6-wayland xdg-desktop-portal-hyprland hyprland hyprlock hyprpaper hyprpolkitagent rofi-wayland waybar kitty starship nwg-look
+sudo pacman -S wget nerd-fonts qt5-wayland qt6-wayland xdg-desktop-portal-hyprland hyprland hyprlock hyprpaper hyprpolkitagent rofi-wayland waybar kitty starship nwg-look thunar parole mousepad
 
 # Configuring starship
 
@@ -11,13 +11,10 @@ echo -e "\e[1;32mConfiguring starship...\e[m"
 shell=$(ps -p $$ -o comm=)
 if [[ $shell == "bash" ]]; then
     echo "eval "$(starship init bash)"" >> ~/.bashrc
-    source .bashrc
 elif [[ $shell == "zsh" ]]; then
     echo "eval "$(starship init zsh)"" >> ~/.zshrc
-    source .zshrc
 elif [[ $shell == "fish" ]]; then
     echo "starship init fish | source" >> ~/.config/fish/config.fish
-    source ~/.config/fish/config.fish
 fi
 
 # Backuping old configs
@@ -38,14 +35,12 @@ cp -rf ~/hgs/hgs-pctrs ~/Pictures
 # Installing gruvbox theme
 
 echo -e "\e[1;32mInstalling theme, icons and cursors...\e[m"
-
-wget -q https://github.com/sainnhe/capitaine-cursors/archive/refs/heads/master.tar.gz
-tar -xf master.tar.gz; rm -rf master.tar.gz; mv -f ~/capitaine-cursors-master/ ~/capitaine
-sudo cp -rf ~/capitaine /usr/share/icons; rm -rf capitaine
-
+git clone -q https://github.com/sainnhe/capitaine-cursors
+mv -f ~/capitaine-cursors ~/capitaine; sudo mv -rf ~/capitaine /usr/share/icons
+git clone -q https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme
+sudo mv -f ~/Gruvbox-GTK-Theme/icons/Gruvbox-Dark /usr/share/icons; rm -rf Gruvbox-GTK-Theme
 cp -rf ~/hgs/dots/gtk-4.0 ~/.config
 
-wget -q https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme/archive/refs/heads/master.tar.gz
-tar -xf master.tar.gz; rm -rf master.tar.gz; sudo mv -f ~/Gruvbox-GTK-Theme-master/icons/Gruvbox-Dark /usr/share/icons; rm -rf Gruvbox-GTK-Theme-master
+# All done
 
 echo -e "\e[1;32mAll done!\e[m"
